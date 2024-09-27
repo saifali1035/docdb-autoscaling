cceb447fcc29de214fcc512919cc325dfa0bbca6
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-09-27 14:02:27 +0530

    Update index.py

--- src/index.py
+++ src/index.py
@@ -9,35 +9,40 @@ max_capacity = int(os.environ.get("max_capacity"))
 cluster_identifier = os.environ.get("cluster_identifier")
 
 def handler(event, context):
-  if min_capacity > max_capacity:
-    logging.critical("The 'min_capacity' cannot be greater than 'max_capacity'.")
-    return None
+    if min_capacity > max_capacity:
+        logging.critical("The 'min_capacity' cannot be greater than 'max_capacity'.")
+        return None
 
-  docdb = autoscaling.DocumentDB(cluster_identifier, min_capacity, max_capacity)
-  replicas_count = docdb.get_replicas_count()
+    try:
+        docdb = autoscaling.DocumentDB(cluster_identifier, min_capacity, max_capacity)
+        replicas_count = docdb.get_replicas_count()
 
-  # Add more replica instances to meet the minimum capacity
-  if min_capacity > replicas_count:
-    logging.warning("Adding more replica instances to meet the minimum capacity...")
-    missing_replicas = min_capacity - replicas_count
+        # Add more replica instances to meet the minimum capacity
+        if min_capacity > replicas_count:
+            logging.info("Adding more replica instances to meet the minimum capacity...")
+            missing_replicas = min_capacity - replicas_count
 
-    while missing_replicas > 0:
-      logging.warning("Adding replica...")
-      docdb.add_replica(ignore_status=True)
-      missing_replicas -= 1
+            while missing_replicas > 0:
+                logging.info("Adding replica...")
+                docdb.add_replica(ignore_status=True)
+                missing_replicas -= 1
 
-    # Ignore the alarm state
-    return None
+            # Ignore the alarm state
+            return None
 
-  sns_message = json.loads(event.get('Records')[0].get('Sns').get('Message'))
-  new_state_value = sns_message.get('NewStateValue')
+        sns_message = json.loads(event.get('Records')[0].get('Sns').get('Message'))
+        new_state_value = sns_message.get('NewStateValue')
 
-  # "In alarm" = scale-out
-  if new_state_value == "ALARM":
-    logging.warning("Adding replica...")
-    docdb.add_replica()
+        # "In alarm" = scale-out
+        if new_state_value == "ALARM":
+            logging.info("Scaling out: Adding replica...")
+            docdb.add_replica()
 
-  # "OK" = scale-in
-  if new_state_value == "OK":
-    logging.warning("Removing replica...")
-    docdb.remove_replica()
+        # "OK" = scale-in
+        elif new_state_value == "OK":
+            logging.info("Scaling in: Removing replica...")
+            docdb.remove_replica()
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return None
